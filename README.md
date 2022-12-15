05c1833c6bff51614831fb9581d0bbfed956ed94
Rhys <rhysfarrer@ITSupports-MacBook-Pro.local>
2022-12-15 12:46:54 +0000

    initial upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Chipseq_analysis_tools
 CAT: ChIPseq Analysis Tools
+
+A work in progress of various scripts that have been useful for analysis for chipseq analysis
---
+++ perl_modules/read_FASTA.pm
@@ -0,0 +1,39 @@
+package fastafile;
+use strict;
+use Bio::SeqIO;
+use Exporter;
+use Encode;
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+$VERSION = 0.1;
+@ISA = qw(Exporter);
+@EXPORT = ();
+@EXPORT_OK = qw();
+%EXPORT_TAGS = (DEFAULT => [qw()], ALL =>[qw()]);
+use FindBin qw($Bin);
+use lib "$Bin";
+use Data::Dumper;
+
+### rfarrer@broadinstitute.org
+
+sub fasta_to_struct {
+	my $input = $_[0];
+	my %struct;
+	$struct{'filename'} = $input;
+	warn "fasta_to_struct: saving from $input...\n";
+	my $inseq = Bio::SeqIO->new('-file' => "<$input",'-format' => 'fasta');
+	while (my $seq_obj = $inseq->next_seq) { 
+		my $id = $seq_obj->id;
+		my $seq = $seq_obj->seq;
+		my $desc = $seq_obj->description;
+		my $length = length($seq);
+		
+		# Save
+		$struct{'seq'}{$id} = $seq;
+		$struct{'desc'}{$id} = $desc;
+		$struct{'seq_length'}{$id} = $length;
+		push @{$struct{'order'}}, $id;
+	}
+	return \%struct;
+}
+
+1;
