75929bc38a629a7bfca9fba7173afec3bd5d0f98
Rhys Farrer <3695082+rhysf@users.noreply.github.com>
2023-06-20 14:51:19 +0100

    updated usage message

--- bash_scripts/chipseq_pipeline_macs2.sh
+++ bash_scripts/chipseq_pipeline_macs2.sh
@@ -8,8 +8,7 @@ source $DIR/Bash_functions/File_input_output.sh
 diemsg() {
 	echo "Usage: $0 -r <treated.BAM> -c <control.BAM>"
 	echo ""
-	echo "Optional: -e Experiment tag [treated_vs_untreated_MACS]"
-	echo "          -i IDR directory [/Users/rhysfarrer/Desktop/Programs/idr-2.0.2/]"
+	echo "Optional: -e Experiment tag [treated_vs_untreated_macs2]"
 	echo "          -n Effective genome size (Gemtools) (Default CNA2 with 150nt reads) [18627656]"
 	echo "          -q Qvalue (For broad marks, you can try 0.05) [0.05]"
 	echo "          -b Broad peaks (y/n) [y]"
@@ -19,11 +18,7 @@ diemsg() {
 	echo "          -t ExtSize (The arbitrary extension size in bp. When nomodel is true, MACS will use this value as fragment size to extend each read towards 3' end) [200]"
 	echo "          -o NoModel (Whether or not to build the shifting model) [n]"
 	echo ""
-	echo "Notes:    >All replicates should be in current working directory, needed for merging files"
-	echo "          >Prerequisites: Bedtools, Samtools and MACS3 are in PATH"
-	echo "          >IDR installed from https://github.com/nboley/idr"
-	echo "          >Cutoff-analysis will analyze number or total length of peaks that can be called by different p-value cutoff then output a summary table to help"
-	echo "          user decide a better cutoff. The table will be saved in NAME_cutoff_analysis.txt file"
+	echo "Notes:    >Prerequisites: bedtools, samtools and macs2 are in PATH"
 	echo "          >Effective genome size calculated with perl script. shortcuts:'hs' for human (2.7e9), 'mm' for mouse (1.87e9), 'ce' for C. elegans (9e7) and 'dm' for fruitfly (1.2e8)"
 	echo "          >MFOLD range Tweaking it is not recommended. However, Too few paired peaks (0) so I can not build the model! Broader your MFOLD range parameter may erase this error. If it still can't build the model, we suggest to use --nomodel and --extsize 147 or other fixed number instead."
 	echo "          >NoModel suggestion use both 'nomodel' and 'extsize' to make signal files from different datasets comparable."
@@ -39,8 +34,7 @@ exists() {
 declare -A opts
 opts[a]="n"
 opts[b]="y"
-opts[e]="treated_vs_untreated_MACS"
-opts[i]="/Users/rhysfarrer/Desktop/Programs/idr-2.0.2/"
+opts[e]="treated_vs_untreated_macs2"
 opts[m]=5
 opts[n]=18627656
 opts[o]="n"
